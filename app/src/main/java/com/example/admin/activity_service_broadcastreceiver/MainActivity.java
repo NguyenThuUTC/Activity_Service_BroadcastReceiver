3d2e88c0dc5bcf2feee03106b36b687479392b4e
NguyenThuUTC <nguyenthu290895@gmail.com>
2017-04-06 03:36:57 +0700

    Tạo class hjjj

--- app/src/main/java/com/example/admin/activity_service_broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/admin/activity_service_broadcastreceiver/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.admin.activity_service_broadcastreceiver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     String msg=" Android: ";
+    Button btnPlay,btnPause,btnStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(msg,"On create");
+
+        anhXa();
+        batSuKien();
+    }
+
+    private void anhXa() {
+        btnPause= (Button) findViewById(R.id.btnPause);
+        btnPlay= (Button) findViewById(R.id.btnPlay);
+        btnStop= (Button) findViewById(R.id.btnStop);
+    }
+
+    private void batSuKien(){
+        btnPlay.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+        btnPause.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +67,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(msg,"OnDestroy");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.btnPlay){
+            Intent play=new Intent(MainActivity.this,ServicePlayMusic.class);
+            startService(play);
+        }else if(v.getId()==R.id.btnPause){
+
+        }else if(v.getId()==R.id.btnStop){
+            Intent stop=new Intent(MainActivity.this,ServicePlayMusic.class);
+            stopService(stop);
+        }
     }
 }
--- app/src/main/java/com/example/admin/activity_service_broadcastreceiver/ServicePlayMusic.java
+++ app/src/main/java/com/example/admin/activity_service_broadcastreceiver/ServicePlayMusic.java
@@ -1,8 +1,41 @@
 package com.example.admin.activity_service_broadcastreceiver;
 
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
 /**
  * Created by admin on 4/5/2017.
  */
 
-public class ServicePlayMusic {
+public class ServicePlayMusic extends Service{
+    MediaPlayer play;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        play=MediaPlayer.create(getApplicationContext(),R.raw.bucthutinhthu2);
+        Toast.makeText(this, "listenning music", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        play.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        play.release();
+        Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+    }
 }
